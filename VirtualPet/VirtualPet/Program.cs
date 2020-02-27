e4df9d2887b5bfe6bdaa98f73eff2f658a3e29bf
Michael Hadley <michaelwesthadley@gmail.com>
2020-02-27 06:46:54 -0600

    Add VirtualPet project from week 1

---
+++ VirtualPet/VirtualPet/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Title = "Pet Sim";
+            ForegroundColor = ConsoleColor.DarkGreen;
+            BackgroundColor = ConsoleColor.White;
+            Clear();
+            WriteLine("Welcome to the Pet Sim!");
+
+            VirtualPet jimothy = new VirtualPet("Jimothy", "Blue", 4);
+            jimothy.Greet();
+            jimothy.Feed("An apple");
+            
+            VirtualPet bubs = new VirtualPet("Bubs", "Black", 2);
+            bubs.Greet();
+            bubs.Feed("Brownie");
+
+            VirtualPet bob = new VirtualPet("Bob", "BluePinkOrange", 0);
+            bob.Greet();
+            bob.Feed("Candy Bar");
+
+            ReadKey();
+        }
+    }
+}
---
+++ VirtualPet/VirtualPet/VirtualPet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using static System.Console;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        // Fields (variables that are defined in a class):
+        public int HappinessLevel;
+        public string Name;
+        public int HungerLevel;
+        public string Color;
+        public bool IsInvicible;
+
+        public VirtualPet()
+        {
+            Name = "Unknown name";
+            Color = "Unknown color";
+            HappinessLevel = 0;
+        }
+
+        public VirtualPet(string name, string color, int happinessLevel)
+        {
+            Name = name;
+            Color = color;
+            HappinessLevel = happinessLevel;
+        }
+
+        public void Greet()
+        {
+            WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            WriteLine("> Hello, I am " + Name + ".");
+            WriteLine("> I am a " + Color + " pet.");
+            WriteLine("> I am a " + HappinessLevel + " happy.");
+            WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        }
+
+        public void Feed(string foodName)
+        {
+            WriteLine("> I am eating " + foodName + ".");
+        }
+    }
+}
